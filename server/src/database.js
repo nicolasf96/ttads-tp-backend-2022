ef255837bf6d504f377e91a241feb83a819b8363
Nicolás Fierro <nico.fierro1@gmail.com>
2023-11-21 18:40:59 -0300

    Database path (edited)

--- server/src/database.js
+++ server/src/database.js
@@ -1,11 +1,11 @@
 import Mongoose from 'mongoose'
 
- const dbconnection =  Mongoose.connect("mongodb://localhost/ttads-tp-backend-2022", {
+ const dbconnection =  Mongoose.connect("mongodb://127.0.0.1:27017/ttads-tp-backend-2022", {
         useUnifiedTopology: true,
         useNewUrlParser: true,
     })
         .then(db => console.log('Db is connected'))
-        .catch(err => console.log('Error '))
+        .catch(err => console.log(err))
 
 
 
